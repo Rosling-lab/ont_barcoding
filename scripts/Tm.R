90289bdea5ba6c37b53eeae786a472b6e2d69d2a
brendanf <brendan.furneaux@gmail.com>
2021-09-06 18:13:31 +0200

    calculations for max and min enthalpy and entropy deltas

--- scripts/Tm.R
+++ scripts/Tm.R
@@ -19,4 +19,28 @@ melt_range <- function(sequence, ...) {
     )
 }
 
-melt_range("ACGACGTTGTAAAAACIMGIGTITCVGAIGTHAARCC", nucleic.acid.conc = 500e-9, hybridisation.type = "dnadna", Mg.conc=0.005)
+enthalpy_range <- function(sequence, ...) {
+    c(
+        min = melting(min_replace(sequence), comp.sequence = min_comp(sequence), ...)$Results$`Enthalpy (J)`,
+        max = melting(max_replace(sequence), comp.sequence = max_comp(sequence), ...)$Results$`Enthalpy (J)`
+    )
+}
+entropy_range <- function(sequence, ...) {
+    c(
+        min = melting(min_replace(sequence), comp.sequence = min_comp(sequence), ...)$Results$`Entropy (J)`,
+        max = melting(max_replace(sequence), comp.sequence = max_comp(sequence), ...)$Results$`Entropy (J)`
+    )
+}
+
+EF1_938f <- "GCYCCYGGHCAYCGTGAYTTYAT"
+EF1_2218r <- "ATGACACCRACRGCRACRGTYTG"
+RPB1_aAf <- "GAGTGTCCGGGGCATTTYGG"
+RPB1_aCr <- "ARAARTCBACHCGYTTBCCCAT"
+bRPB2_6F <- "TGGGGYATGGTITGYCCYGC"
+bRPB2_7R <- "GAYTGRTTRTGRTCRGGGAAVGG"
+fRPB2_5F <- "GAYGAYMGWGATCAYTTYGG"
+bRPB2_6R2 <- "GGRCAIACCATICCCCARTG"
+
+melt_range(RPB1_aCr, nucleic.acid.conc = 500e-9, hybridisation.type = "dnadna", Mg.conc=0.005)
+enthalpy_range(bRPB2_6R2, nucleic.acid.conc = 500e-9, hybridisation.type = "dnadna", Mg.conc=0.005)
+entropy_range(bRPB2_6R2, nucleic.acid.conc = 500e-9, hybridisation.type = "dnadna", Mg.conc=0.005)
